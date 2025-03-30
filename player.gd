077a60c0d8b4eea2d305c45b866f159a846761d9
nathanael early <nathanael.early@gmail.com>
2025-03-30 15:56:54 -0400

    audio/animations
    added audio animations and fixed collision :)

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-var personl1 = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -15,12 +15,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("runleft", "runright")
+	
+	# Flipthe Sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
 	if direction:
 		velocity.x = direction * SPEED
 	else:
