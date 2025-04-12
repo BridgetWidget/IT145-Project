1c4d598e93205803f5afacdf2b3e17d3b25189a8
BridgetWidget <bridget@longstring.com>
2025-04-12 16:00:52 -0400

    Coyote time, base level, and level exits

---
+++ IT145-Project-coyote-time/Assets/Scripts/bullet.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@export var speed = 100  # Increase speed if necessary
+var bullettype
+
+func _ready():
+	set_as_top_level(true)
+	#put some things to change sprite based on input(i want 
+	#to make a function in bullet that gets called by the parent that way when you put
+	# bullet inside of the enemy it can change the sprite and its corresponding damaging
+	# or ability)
+
+func _physics_process(delta):
+	position += Vector2.RIGHT.rotated(rotation) * speed * delta  # Move in the correct direction
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_bullet_body_entered(body):
+	if is_in_group("enemy_bullet") and body.is_in_group("player"):
+		body.kill()
+	
+	if body != get_parent():
+		queue_free()
+
+func _on_enemy_bullet_area_entered(area):
+	_on_bullet_body_entered(area)
---
+++ IT145-Project-coyote-time/Assets/Scripts/game_manager.gd
@@ -0,0 +1,23 @@
+#BR - this script will be autoloaded and will manage transitions
+extends Node
+
+
+
+var current_area = 1
+var area_path = "res://Assets/Scenes/Areas/"
+
+func next_level():
+	current_area += 1
+	var full_path = area_path + "area_" + str(current_area) + ".tscn"
+	get_tree().change_scene_to_file(full_path)
+	print("The player has moves to area " + str(current_area))
+	
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
