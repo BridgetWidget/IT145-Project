9fb2a53635304d113b73d2ec22da2f3fdfab9643
nathanael early <nathanael.early@gmail.com>
2025-04-12 22:26:06 -0400

    Working enemy
    hehe haha finally i need to more tonight

--- Enemy.gd
+++ Enemy.gd
@@ -1,19 +1,31 @@
 extends CharacterBody2D
 
-@export var speed = 130
-@export var flip = false
-var move = Vector2(speed,10)
-@onready var tile_map = get_parent().get_node("TileMap")
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction =1
-@onready var animated_sprite_2d = $AnimatedSprite2D
+var speed = 60
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var facing_right = false
 
 func _physics_process(delta):
+	
 	if not is_on_floor():
-		velocity.y += gravity * delta * speed
-	position.x += delta * direction
-	if direction > 0:
-		animated_sprite_2d.flip_h = false
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
+		velocity.y += gravity * delta
+		
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+		
+	velocity.x = speed
+	move_and_slide()
+func flip():
+	facing_right = !facing_right
+	
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		speed = abs(speed) * -1
+	else:
+		speed = abs(speed)
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("tile"):
+		flip()
