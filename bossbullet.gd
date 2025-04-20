efd8ac2cf08291bd9fc45237981742200dc738e9
nathanael early <nathanael.early@gmail.com>
2025-04-19 22:12:26 -0400

    Boss.attacks
    made a boss that can attack in his pew pew blue WE SPENT 8 HOURS I CAN BE SILLY :)

--- bossbullet.gd
+++ bossbullet.gd
@@ -8,16 +8,11 @@ extends Area2D
 var bullettype
 var Bullet_object
 var rng = RandomNumberGenerator.new()
-var Type_of_bullet = floor(randf_range(1.0 , 3.99)) #= rng.randf_range(1, 3)
+var Type_of_bullet =3
 var direc = 1
 
 func _ready():
 	set_as_top_level(true)
-	if Type_of_bullet == 1.0:# Make switch statements
-		animated_sprite_2d.play("pink")
-		Bullet_object = EPinkBullet.new()
-	if Type_of_bullet == 2.0:
-		animated_sprite_2d.play("default")
 	if Type_of_bullet == 3.0:
 		animated_sprite_2d.play("blue")
 		Bullet_object = EBlueBullet.new()
@@ -34,18 +29,6 @@ func _on_bullet_body_entered(body):
 	if body.is_in_group("bullet"):
 		self.queue_free
 		body.queue_free()
-	elif Type_of_bullet == 1:
-		Bullet_object.PinkBullet(body,$".")
-	elif Type_of_bullet == 2:
-		NormalBullet(body)
 	elif Type_of_bullet == 3:
 		Bullet_object.BlueeBullet(body,$".")
 # if sprite is = to blagh blagh and blagh it calls different boobs
-		
-func NormalBullet(body):
-	if body.is_in_group("player"):
-		self.queue_free()
-		Game.Death += 1
-		get_tree().reload_current_scene()
-	if body != get_parent():
-		queue_free()
